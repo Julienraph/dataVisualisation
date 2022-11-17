1f0ad1b65654e98edcb8f1111fb6601139cb6052
julienraph <rjulien@digitec.fr>
2022-11-17 16:14:23 +0100

    Piechart corrigé

--- scriptJS.js
+++ scriptJS.js
@@ -55,6 +55,7 @@ var rangeColor = []
 var domainColor
 var currentDomainGenre
 var dataTSV
+var countriesDataMap 
 const loadAndProcessData = () =>
 Promise.all([
   //d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.json'),
@@ -78,6 +79,7 @@ Promise.all([
     tsvCountryData.forEach(d => {
       rowByIdCountry[d.iso_n3] = d;
     });
+    countriesDataMap = rowByIdCountry 
     countries.features.forEach(d => {
       var newdata = tsvData.filter(function(row) {return row.goodISO == d.id && row.goodDate == year && row.goodGeneralGenre != "Other" && row.goodGeneralGenre != "Undefined"});
       var max = d3.max(newdata, function(row) { return +row.deezerFans});
@@ -136,6 +138,9 @@ slider.oninput = function() {
   loadAndProcessData().then(countries => {
     currentDomainGenre = [...new Set(countries.features.map(item => item.properties.goodGeneralGenre))]
     features = countries.features;
+    if(selectedCountryId != null) {
+    createPieChart(selectedCountryId)
+    }
     render();
   });
 }
@@ -151,7 +156,8 @@ const onClick = d => {
 const onCountryClick = id => {
   if(id != null) {
     selectedColorValue = null
-    openNav(id);
+    openNav();
+    createPieChart(id)
   } else {
     closeNav()
   }
@@ -264,28 +270,11 @@ countryPaths
   document.getElementById("svgdivid").style.marginLeft = "0";
 }
 
-function openNav(countryID) {
- // set the dimensions and margins of the graph
-
- var data1 = {}
- var nomCountry
- var newdata = dataTSV.filter(function(row) {return row.goodISO == countryID && row.goodDate == year && row.goodGeneralGenre != "Other" && row.goodGeneralGenre != "Undefined"});
- newdata.sort(function(a,b) {
-  return a.deezerFans - b.deezerFans;
-  });
- newdata.forEach(d => {
- nomCountry = d.goodCountry
-  data1[d.goodGeneralGenre] = d.deezerFans
- });
  
  var widthPie = 450
  var heightPie = 450
  var marginPie = 40
- //plusieurs meme genre "pop", regrouper et sum deezerfans ?
- document.getElementById("titre").innerHTML  = nomCountry
- document.getElementById("mySidepanel").style.width = "500px";
- document.getElementById("mySidepanel").style.height = "100%";
- document.getElementById("svgdivid").style.marginLeft = "500px"
+
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(widthPie, heightPie) / 2 - marginPie
@@ -298,18 +287,43 @@ var svgPiechart = d3.select("#mySidepanel")
 .append("g")
 .attr("transform", "translate(" + widthPie / 2 + "," + heightPie / 2 + ")");
 
-
-var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
-
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(data1)
-  .range(d3.schemeSet2);
-
   var arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(radius)
 
+  function openNav(countryID) {
+  document.getElementById("mySidepanel").style.width = "500px";
+  document.getElementById("mySidepanel").style.height = "100%";
+  document.getElementById("svgdivid").style.marginLeft = "500px" 
+  }
+
+  function createPieChart(countryID) {
+ // set the dimensions and margins of the graph
+
+ var data1 = {}
+ var isUndefined = false;
+ var nomCountry
+ var newdata = dataTSV.filter(function(row) {return row.goodISO == countryID && row.goodDate == year && row.goodGeneralGenre != "Other" && row.goodGeneralGenre != "Undefined"});
+ newdata.forEach(d => {
+ nomCountry = d.goodCountry
+  data1[d.goodGeneralGenre] = d.deezerFans
+ });
+  if(Object.keys(data1).length === 0) {
+    nomCountry = countriesDataMap[countryID].name_long
+    data1["Undefined"] = 1000
+    isUndefined = true;
+  }
+
+  // Get an array of the keys:
+let keysGenre = Object.keys(data1);
+
+// Then sort by using the keys to lookup the values in the original object:
+keysGenre.sort(function(a, b) { return data1[b] - data1[a] });
+
+
+console.log(keysGenre);
+  //plusieurs meme genre "pop", regrouper et sum deezerfans ?
+  document.getElementById("titre").innerHTML  = nomCountry 
 // A function that create / update the plot for a given variable:
 function update(data) {
 
@@ -319,7 +333,7 @@ var pie = d3.pie()
 var data_ready = pie(d3.entries(data))
 
 // map to data
-var u = svgPiechart.selectAll("mySlices")
+var u = svgPiechart.selectAll("path")
 .data(data_ready)
 
 // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
@@ -328,19 +342,24 @@ u
 .append('path')
 .merge(u)
 .transition()
-.duration(1000)
+.duration(500)
 .attr('d', arcGenerator
 )
-.attr('fill', function(d){ return(color(d.data.key)) })
+.attr('fill', function(d){if(d.data.key != "Undefined") {
+  return(colorScale(d.data.key))
+  } else {
+    return "rgb(107,107,107)"
+  }})
 .attr("stroke", "white")
 .style("stroke-width", "0.1px")
 .style("opacity", 1)
-
 // Now add the annotation. Use the centroid method to get the best coordinates)
-u
+
+v = svgPiechart.selectAll("text").data(data_ready)
+v
   .enter()
-  .append('text')
-  .merge(u)
+  .append("text")
+  .merge(v)
   .text(function(d){ return d.data.key})
   .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
   .style("text-anchor", "middle")
@@ -351,42 +370,35 @@ u
 .exit()
 .remove()
 
+v
+.exit()
+.remove()
+
 }
 
 // Initialize the plot with the first dataset
 update(data1)
 
+if (document.getElementById("proList") != null) {
+  document.getElementById("proList").remove()
+}
+if(!isUndefined) {
 createLi(data1)
+}
 
 function createLi(data){
-  if (document.getElementById("proList") != null) {
-    document.getElementById("proList").remove()
-  }
-  var ul = document.createElement('ul');
+  var ul = document.createElement('ol');
   ul.setAttribute('id','proList');
 
-  productList = ['Electronics Watch','House wear Items','Kids wear','Women Fashion'];
-
-  console.log(data)
   document.getElementById('mySidepanel').appendChild(ul);
 
-  for (var key of Object.keys(data)) {
-    console.log(key + " -> " + data[key])
+  for (var key of keysGenre) {
     var li = document.createElement('li');
     li.setAttribute('class','item');
 
     ul.appendChild(li);
 
     li.innerHTML= key + " a " + data[key] + " fans"  ;
-}
-
-  function renderProductList(element, index, arr) {
-      var li = document.createElement('li');
-      li.setAttribute('class','item');
-
-      ul.appendChild(li);
-
-      li.innerHTML=li.innerHTML + element;
   }
 }
 
